aa81126d59629d300ba6accdbac3f052a0125ac1
Squatingyeti <squatingyeti@yeticraft.net>
2012-03-23 00:50:27 -0500

    extra derp whois is already used as a command

--- src/net/yeticraft/squatingyeti/GetInfo/GetInfo.java
+++ src/net/yeticraft/squatingyeti/GetInfo/GetInfo.java
@@ -1,4 +1,4 @@
-package net.yeticraft.squatingyeti.WhoIs;
+package net.yeticraft.squatingyeti.GetInfo;
 
 import java.util.logging.Logger;
 
@@ -8,47 +8,53 @@ import net.milkbowl.vault.permission.Permission;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class WhoIs extends JavaPlugin {
+public class GetInfo extends JavaPlugin {
 	
 	Logger log = Logger.getLogger("Minecraft");
 	private static Permission permissions = null;
 	private static boolean usingVault = false;
 	private static boolean usingPermissions = false;
 
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args, Player target) {
+	public boolean onCommand(CommandSender sender, Command command, String label, Entity[] args){
 		if (!(sender instanceof Player)) {
 			sender.sendMessage("This command only works in-game");
 			return true;
 		}
 		
 		Player player = (Player) sender;
-		String targetName = args[0].toLowerCase();
-		Player oPlayer = (Player) target;
+		Entity oPlayer = args[0];
+		String targetName = ((Player) oPlayer).getName();
 		if (args.length == 0 ) {
 			sender.sendMessage(ChatColor.YELLOW + "You must specify a player");
 			return true;
 		}
 		if (args.length >= 2) {
-			sender.sendMessage(ChatColor.YELLOW + "Too many arguments. Try /whois help");
+			sender.sendMessage(ChatColor.YELLOW + "Too many arguments. Try /GetInfo [name]");
 			return true;
 		}
-		if (label.equalsIgnoreCase("whois")) {
-			
+		if (label.equalsIgnoreCase("getinfo")) {
+			player.sendMessage("We are working to here " + oPlayer);
+			return true;
 		}
+		else {
+			player.sendMessage("Something went wrong");
+		}
+		return false;
 	}
 	
 	
 	public void onDisable() {
-		log.info("WhoIs has been disabled");
+		log.info("GetInfo has been disabled");
 	}
 	
 	public void onEnable() {
-		log.info("WhoIs has been enabled");
+		log.info("GetInfo has been enabled");
 		initializeVault();
 	}
 	public static Permission getPermissions() {
@@ -80,17 +86,17 @@ public class WhoIs extends JavaPlugin {
 	private void initializeVault() {
 		Plugin x = this.getServer().getPluginManager().getPlugin("Vault");
 		if (x != null && x instanceof Vault) {
-			log.info("[WhoIs] found [Vault] and searching for economy plugin.");
+			log.info("[GetInfo] found [Vault] and searching for permissions plugin.");
 			usingVault = true;
 			if (setupPermissions()) {
-				log.info("[WhoIs] found [" + permissions.getName() + "] plugin. Prices enabled.");
+				log.info("[GetInfo] found [" + permissions.getName() + "] plugin. Permissions enabled.");
 			}
 				else if (!setupPermissions()) {
-					log.severe("[WhoIs] permissions plugin not found.");
+					log.severe("[GetInfo] permissions plugin not found.");
 					usingPermissions = false;
 				}
 		}else {
-			log.severe("[WhoIs] could not find [Vault]!");
+			log.severe("[GetInfo] could not find [Vault]!");
 			usingVault = false;
 		}
 	}
