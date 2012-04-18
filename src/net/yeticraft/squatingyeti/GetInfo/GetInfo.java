c78c48a5bb41b86a0ce97cad35accd192d173983
Squatingyeti <squatingyeti@yeticraft.net>
2012-04-18 13:45:54 -0500

    Change name of project and get it working

--- src/net/yeticraft/squatingyeti/GetInfo/GetInfo.java
+++ src/net/yeticraft/squatingyeti/GetInfo/GetInfo.java
@@ -4,32 +4,39 @@ import java.util.logging.Logger;
 
 import net.milkbowl.vault.Vault;
 import net.milkbowl.vault.permission.Permission;
-
+import net.yeticraft.squatingyeti.GetZone.GetZone;
+import net.yeticraft.squatingyeti.YetiHome.*;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
 public class GetInfo extends JavaPlugin {
-	
-	Logger log = Logger.getLogger("Minecraft");
+
+	public HomeManager homes;
+	public GetZone zones;
+	public Logger log = Logger.getLogger("Minecraft");
 	private static Permission permissions = null;
 	private static boolean usingVault = false;
 	private static boolean usingPermissions = false;
 
-	public boolean onCommand(CommandSender sender, Command command, String label, Entity[] args){
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
 		if (!(sender instanceof Player)) {
 			sender.sendMessage("This command only works in-game");
 			return true;
 		}
 		
 		Player player = (Player) sender;
-		Entity oPlayer = args[0];
-		String targetName = ((Player) oPlayer).getName();
+		Date firstPlayed;
+		Date lastPlayed;
 		if (args.length == 0 ) {
 			sender.sendMessage(ChatColor.YELLOW + "You must specify a player");
 			return true;
@@ -37,18 +44,73 @@ public class GetInfo extends JavaPlugin {
 		if (args.length >= 2) {
 			sender.sendMessage(ChatColor.YELLOW + "Too many arguments. Try /GetInfo [name]");
 			return true;
-		}
-		if (label.equalsIgnoreCase("getinfo")) {
-			player.sendMessage("We are working to here " + oPlayer);
-			return true;
-		}
-		else {
-			player.sendMessage("Something went wrong");
-		}
-		return false;
-	}
-	
-	
+		} 
+		if (label.equalsIgnoreCase("getinfo") && args.length == 1) {
+			Player target = findOnlinePlayer(args[0]);
+			
+            if (target != null) {
+            	//target = findOnlinePlayer(args[0]);
+                Player sendPlayer = (Player) sender;
+                log.info("Sender: " +player);
+                String owner = target.getName();
+                log.info(owner);
+                float exp = target.getExp();
+                Location loc = target.getLocation();
+                log.info("loc: " +loc);
+                int x = target.getLocation().getBlockX();
+                int y = target.getLocation().getBlockY();
+                int z = target.getLocation().getBlockZ();
+                int health = target.getHealth();
+                short playDays;
+                //String zone = zones.currentZone(loc, owner);
+                firstPlayed = new Date (target.getFirstPlayed());
+				lastPlayed = new Date(target.getLastPlayed());
+				playDays = (short)((System.currentTimeMillis() - target.getFirstPlayed()) / 86400000);
+					player.sendMessage(ChatColor.GREEN + owner);
+					player.sendMessage(ChatColor.YELLOW + "================================");
+					player.sendMessage("First Played " +(new SimpleDateFormat()).format(firstPlayed));
+					player.sendMessage("Last Played " + (new SimpleDateFormat()).format(lastPlayed));
+					player.sendMessage("Location: x:" + x + " y:" + y + " z:" + z);
+					player.sendMessage("Zone: " +  zones.getZoneOther(player, target));
+					player.sendMessage("Days Played: " + playDays);
+					player.sendMessage("Health: " + health);
+					player.sendMessage("Experience: " + exp);
+					sendPlayer.performCommand("listhomes" + owner);
+					return true;
+				}
+            
+
+		else if (target == null) {
+				OfflinePlayer oPlayer = getServer().getOfflinePlayer(args[0]);
+			if (!oPlayer.hasPlayedBefore()){
+					player.sendMessage(ChatColor.RED + args[0] + " has not played on Yeticraft before");
+					return true;
+			}		
+					String offPlayer = oPlayer.getName();
+					firstPlayed = new Date (oPlayer.getFirstPlayed());
+					lastPlayed = new Date(oPlayer.getLastPlayed());
+					short playDays;
+					playDays = (short)((System.currentTimeMillis() - oPlayer.getFirstPlayed()) / 86400000);
+					player.sendMessage(ChatColor.GREEN + offPlayer);
+					player.sendMessage(ChatColor.YELLOW + "================================");
+					player.sendMessage(ChatColor.GREEN + offPlayer + ChatColor.RED + " is not online");
+					player.sendMessage("First Played " +(new SimpleDateFormat()).format(firstPlayed));
+					player.sendMessage("Last Played " + (new SimpleDateFormat()).format(lastPlayed));
+					player.sendMessage("Days Played: " + playDays);
+					return true;
+			}
+		} return false;
+	}	
+
+
+
+
+	/*public static getOffline(OfflinePlayer offPlayer, String[] args) {
+		offPlayer = Bukkit.getOfflinePlayer(args[0]);
+		String offTarget = null;
+		offTarget = offPlayer.getName();
+		if ()
+	} */
 	public void onDisable() {
 		log.info("GetInfo has been disabled");
 	}
@@ -99,5 +161,25 @@ public class GetInfo extends JavaPlugin {
 			log.severe("[GetInfo] could not find [Vault]!");
 			usingVault = false;
 		}
+	}
+	
+	public Player findOnlinePlayer(String name) {
+		if (name != null) {
+			Player p = getServer().getPlayer(name);
+			if (p != null) return p;
+			//try to find by partial name
+			Boolean found = false;
+			int nameLength = name.length();
+			for (Player p1: getServer().getOnlinePlayers()) {
+				if (p1.getName().length() > nameLength) {
+					if (p1.getName().substring(0, (nameLength - 1)).equalsIgnoreCase(name)) {
+						//return null if more than 1 player matches
+						if (found) return null;
+						found = true;
+						p = p1;
+					}
+				}	
+			} return p;
+		} return null;
 	}
 }
